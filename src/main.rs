a10f0e481d5b226b4b633acd3c5c97e10df9b62b
EmadDeve20 <emaddeve20@gmail.com>
2022-04-07 12:06:41 +0430

    Following the big change 	We defined the program_file variable as changeable. To constantly change the line under review and execute code line by line. 	The "curent_line" variable in the "ProgramFile" data structure maintains the line in question.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use kha::{ProgramFile, line_reader};
 fn main() {
     let file_path = env::args().nth(1).unwrap();
 
-    let program_file = ProgramFile::new(&file_path).unwrap_or_else(|err| {
+    let mut program_file = ProgramFile::new(&file_path).unwrap_or_else(|err| {
         eprintln!("file error: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = line_reader(&program_file) {
+    if let Err(e) = line_reader(&mut program_file) {
         eprintln!("{}", e.get_format_message());
         process::exit(1);
     }
