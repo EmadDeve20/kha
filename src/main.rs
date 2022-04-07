8b31d46663f25996fe1c8a6185844eedd938c85d
EmadDeve20 <emaddeve20@gmail.com>
2022-04-07 09:30:17 +0430

    adding new features: 	1- kha program lnaguage now, can shows syntax error with line 	2- kha programing language now, have comment '#'

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 use std::{env, error::Error, fs, process};
 
 #[derive(Debug)]
-enum Erros {
+enum ErroType {
     CommondNotFound,
+}
+#[derive(Debug)]
+struct KhaInterpreterErro {
+    type_of_erro: ErroType,
+    line: String,
+    text_of_line: String,
+}
+
+impl KhaInterpreterErro {
+    fn new(type_of_erro: ErroType, line: String, text_of_line: String) -> KhaInterpreterErro {
+        KhaInterpreterErro {
+            type_of_erro,
+            line,
+            text_of_line,
+        }
+    }
+
+    fn get_format_message(&self) -> String {
+        format!(
+            "{:?}: {} in {} line",
+            self.type_of_erro, self.text_of_line, self.line
+        )
+    }
 }
 
 struct ProgramFile {
@@ -26,24 +49,29 @@ fn main() {
     });
 
     if let Err(e) = line_reader(&program_file) {
-        eprintln!("Error: {:?}", e);
+        eprintln!("{}", e.get_format_message());
         process::exit(1);
     }
 }
 
-fn line_reader(code: &ProgramFile) -> Result<(), Erros> {
+fn line_reader(code: &ProgramFile) -> Result<(), KhaInterpreterErro> {
+    let mut curent_line: u32 = 1;
     for line in code.syntaxt.lines() {
-        parser(line)?;
+        parser(line, &curent_line)?;
+        curent_line += 1;
     }
 
     Ok(())
 }
 
-fn parser(line: &str) -> Result<(), Erros> {
-    let line_as_byte = line.as_bytes();
-    let syntax = &line.clone();
+fn parser(text: &str, line: &u32) -> Result<(), KhaInterpreterErro> {
+    let line_as_byte = text.clone().as_bytes();
+    let syntax = &text.clone();
 
     for (i, &item) in line_as_byte.iter().enumerate() {
+        if item == b'#' {
+            return Ok(());
+        }
         if item == b' ' {
             if &syntax[..i] == "print" {
                 print_commond(&syntax[i + 1..]);
@@ -51,7 +79,11 @@ fn parser(line: &str) -> Result<(), Erros> {
             }
         }
     }
-    Err(Erros::CommondNotFound)
+    Err(KhaInterpreterErro::new(
+        ErroType::CommondNotFound,
+        line.to_string(),
+        text.to_string(),
+    ))
 }
 
 fn print_commond(command: &str) {
