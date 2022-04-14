25257abbd0eae7fb03b456ad7d03626c7acff69f
EmadDeve20 <emaddeve20@gmail.com>
2022-04-14 10:05:45 +0430

    using just one line (12)

--- src/commands.rs
+++ src/commands.rs
@@ -8,10 +8,8 @@ pub fn exit_command() {
     process::exit(0);
 }
 
-pub fn go_command(current_line:&mut usize, go_line: &String, line_total: &usize) {
-    *current_line = go_line
-        .parse::<usize>()
-        .unwrap_or_else(|_| line_total + 1);
+pub fn go_command(current_line: &mut usize, go_line: &String, line_total: &usize) {
+    *current_line = go_line.parse::<usize>().unwrap_or_else(|_| line_total + 1);
     if *current_line == 0 {
         *current_line = line_total + 1
     }
