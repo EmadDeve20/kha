662e176cb24faa5548383b0ff4201e70361e363f
EmadDeve20 <emaddeve20@gmail.com>
2022-05-06 14:18:49 +0430

    update
    1- create the "run_online_interpreter" function to run online interpreter if user run kha program without any argument
    2- import the "get_plain_text" function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use kha::{line_reader, ProgramFileConfig};
-use std::{env, process};
+use kha::{get_plain_text, line_reader, ProgramFileConfig};
+use std::{env, io, process};
 
 fn main() {
-    let mut program_file = ProgramFileConfig::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("file error: {}", err);
-        process::exit(1);
-    });
+    if env::args().len() == 1 {
+        run_online_interpreter();
+    } else {
+        let mut program_file = ProgramFileConfig::new(env::args()).unwrap_or_else(|err| {
+            eprintln!("file error: {}", err);
+            process::exit(1);
+        });
 
-    if let Err(e) = line_reader(&mut program_file) {
-        eprintln!("{}", e.get_format_message());
-        process::exit(1);
+        if let Err(e) = line_reader(&mut program_file) {
+            eprintln!("{}", e.get_format_message());
+            process::exit(1);
+        }
+    }
+}
+
+fn run_online_interpreter() {
+    loop {
+        // TODO: is why this [In] can not print first? we must fix it
+        // println!("[In]: ");
+        let mut text = String::new();
+        io::stdin().read_line(&mut text).expect("sonething wrong!");
+        print!("[OutPut]: ");
+        get_plain_text(text);
     }
 }
