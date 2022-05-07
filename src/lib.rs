929b505bb76d5e95875dc523e339a4b541f01851
EmadDeve20 <emaddeve20@gmail.com>
2022-05-07 05:34:22 +0430

    big Update
    1- change the type of line in the "KhaInterpreterErro" struct to usize
    2- create the "new_line" fucntion for adding a new line to "ProgramFileConfig"
    3- change the name of the "line_reader" function to the "interpreter"
    4- remove the "parser" function and change the name of "parseer" to "parser"
    5- remove the "get_plain_text" function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{env, error::Error,fs, thread, time::Duration, vec};
+use std::{env, error::Error, fs, thread, time::Duration, vec};
 
 mod commands;
 
@@ -9,12 +9,12 @@ pub enum ErroType {
 #[derive(Debug)]
 pub struct KhaInterpreterErro {
     type_of_erro: ErroType,
-    line: String,
+    line: usize,
     text_of_line: String,
 }
 
 impl KhaInterpreterErro {
-    pub fn new(type_of_erro: ErroType, line: String, text_of_line: String) -> KhaInterpreterErro {
+    pub fn new(type_of_erro: ErroType, line: usize, text_of_line: String) -> KhaInterpreterErro {
         KhaInterpreterErro {
             type_of_erro,
             line,
@@ -47,6 +47,7 @@ impl ProgramFileConfig {
             Some(arg) => arg.trim().parse::<u64>().unwrap_or_else(|_| 1),
             None => 0,
         };
+
         let syntax = get_list_of_lines(&text_file);
         let line_total = syntax.len();
         Ok(ProgramFileConfig {
@@ -56,10 +57,27 @@ impl ProgramFileConfig {
             delay,
         })
     }
+
+    pub fn new_line(&mut self, new_text: String) -> &Self {
+        self.syntax.push(new_text);
+        self.line_total += 1;
+
+        self
+    }
+}
+
+pub fn new_empy_programing_config() -> ProgramFileConfig {
+    ProgramFileConfig {
+        delay: 0,
+        curent_line: 0,
+        line_total: 1_000_000,
+        syntax: vec![],
+    }
 }
 
 fn get_list_of_lines(text: &String) -> Vec<String> {
     let mut list_of_lines: Vec<String> = Vec::new();
+    // I pushed an empty string in the first index because we start at one line and we have no zero line in plain text
     list_of_lines.push("".to_string());
 
     for line in text.lines() {
@@ -70,83 +88,70 @@ fn get_list_of_lines(text: &String) -> Vec<String> {
     list_of_lines
 }
 
-pub fn line_reader(code: &mut ProgramFileConfig) -> Result<(), KhaInterpreterErro> {
+pub fn interpreter(code: &mut ProgramFileConfig) -> Result<(), KhaInterpreterErro> {
+    let lex = lexer(code.syntax.clone());
+    let parse = parser(lex);
     loop {
         if code.curent_line >= code.line_total {
             return Ok(());
         }
-        parser(
-            &code.syntax[code.curent_line],
+
+        let parse = &parse[code.curent_line];
+
+        evaluation(
+            parse.to_vec(),
             &mut code.curent_line,
             &code.line_total,
+            &code.delay,
         )?;
-        thread::sleep(Duration::from_secs(code.delay));
     }
 }
 
-fn parser(text: &String, line: &mut usize, max_line: &usize) -> Result<(), KhaInterpreterErro> {
-    if text.trim() == "exit" {
-        commands::exit_command();
-        return Ok(());
-    }
-    let line_as_byte = text.as_bytes();
-    let syntax = &text.clone();
+pub fn online_interpreter(code: &mut ProgramFileConfig) -> Result<(), KhaInterpreterErro> {
+    let lex = lexer(code.syntax.clone());
+    let parse = parser(lex);
+    let parse = &parse[code.curent_line];
 
-    for (i, &item) in line_as_byte.iter().enumerate() {
-        if item == b'#' {
-            *line += 1;
-            return Ok(());
-        }
-        if item == b' ' {
-            if &syntax[..i] == "print" {
-                commands::print_commond(&syntax[i + 1..]);
-                *line += 1;
-                return Ok(());
-            }
-            if &syntax[..i] == "go" {
-                commands::go_command(line, &syntax[i + 1..].to_string(), &max_line);
-                return Ok(());
-            }
-        }
-    }
-    Err(KhaInterpreterErro::new(
-        ErroType::CommondNotFound,
-        line.to_string(),
-        text.to_string(),
-    ))
+    evaluation(
+        parse.to_vec(),
+        &mut code.curent_line,
+        &code.line_total,
+        &code.delay,
+    )?;
+
+    Ok(())
 }
 
 // this function is special string splitter for kha
-fn kha_splitter<'a>(text: &'a str) -> Vec<&'a str> {
+fn kha_splitter(text: String) -> Vec<String> {
     let text_as_bytes = text.as_bytes();
 
-    // this character is a comment so we return an empty string
-    if text_as_bytes[0] == b'#' {
-        return vec![""];
-    }
-
-    let mut result: Vec<&str> = Vec::new();
+    let mut result: Vec<String> = Vec::new();
 
     for (index, &item) in text_as_bytes.into_iter().enumerate() {
+        // this character is a comment so we return an empty string
+        if item == b'#' {
+            return vec![String::from("comment")];
+        }
         if item == b' ' {
-            result.push(&text[..index].trim());
-            result.push(&text[index + 1..].trim());
+            result.push(text[..index].trim().to_string());
+            result.push(text[index + 1..].trim().to_string());
             return result;
         } else if item == b'=' {
-            result.push(&text[..index].trim());
-            result.push("=");
-            result.push(&text[index + 1..].trim());
+            result.push(text[..index].trim().to_string());
+            result.push(String::from("="));
+            result.push(text[index + 1..].trim().to_string());
             return result;
         }
     }
 
-    vec![&text]
+    vec![text]
 }
 
-fn lexer<'a>(text: &'a str) -> Vec<Vec<&'a str>> {
+fn lexer(syntax: Vec<String>) -> Vec<Vec<String>> {
     let mut lex = Vec::new();
 
-    for t in text.lines() {
+    for t in syntax {
         lex.push(kha_splitter(t));
     }
 
@@ -154,26 +159,30 @@ fn lexer<'a>(text: &'a str) -> Vec<Vec<&'a str>> {
 }
 
 //TODO: We must change this name of function to 'parser'
-fn parseer<'a>(lex: Vec<Vec<&'a str>>) -> Vec<Vec<&'a str>> {
-    let mut parse: Vec<Vec<&'a str>> = Vec::new();
+fn parser(lex: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    let mut parse: Vec<Vec<String>> = Vec::new();
 
     for list in lex {
-        let mut p = Vec::new();
+        let mut p: Vec<String> = Vec::new();
 
         if list.len() == 3 {
             if list[1] == "=" {
-                p.push("var");
-                p.push(list[0]);
-                p.push(list[2]);
+                p.push(String::from("var"));
+                p.push(list[0].to_string());
+                p.push(list[2].to_string());
             }
         }
         if list.len() == 2 {
-            p.push("command");
-            p.push(list[0]);
-            p.push(list[1]);
-        } else if list.len() == 1 {
-            p.push("command");
-            p.push(list[0]);
+            p.push(String::from("command"));
+            p.push(list[0].to_string());
+            p.push(list[1].to_string());
+        }
+        if list.len() == 1 && list[0] != "comment".to_string() {
+            p.push(String::from("command"));
+            p.push(list[0].to_string());
+        }
+        if list.len() == 1 && (list[0] == "comment".to_string() || list[0] == "") {
+            p.push("comment".to_string());
         }
 
         parse.push(p);
@@ -182,28 +191,47 @@ fn parseer<'a>(lex: Vec<Vec<&'a str>>) -> Vec<Vec<&'a str>> {
     parse
 }
 
-fn evaluation<'a>(parse: Vec<Vec<&'a str>>) {
-    for (_, vec) in parse.iter().enumerate() {
-        if vec.len() == 3 {
-            if vec[1] == "print" {
-                commands::print_commond(vec[2]);
-            }
-        }
-        if vec.len() == 2 {
-            if vec[1] == "exit" {
-                commands::exit_command();
-            }
-            if vec[1] == "clear" {
-                commands::clear_command();
-            }
-        }
-
-        // println!("{:#?}", vec);
+fn evaluation(
+    parse: Vec<String>,
+    curent_line: &mut usize,
+    total_line: &usize,
+    delay: &u64,
+) -> Result<(), KhaInterpreterErro> {
+    if parse[0] == "comment".to_string() {
+        *curent_line += 1;
+        return Ok(());
     }
-}
 
-pub fn get_plain_text(plain_text: String) {
-    evaluation(parseer(lexer(&plain_text[..])))
+    thread::sleep(Duration::from_secs(*delay));
+
+    if parse[0] == "command".to_string() {
+        if parse[1] == "print".to_string() {
+            commands::print_commond(&parse[2]);
+            *curent_line += 1;
+            return Ok(());
+        }
+        if parse[1] == "go".to_string() {
+            commands::go_command(curent_line, &parse[2].to_string(), &total_line);
+            return Ok(());
+        }
+        if parse[1] == "exit".to_string() || parse[1] == "exit\n".to_string() {
+            commands::exit_command();
+            *curent_line += 1;
+            return Ok(());
+        }
+        if parse[1] == "clear".to_string() || parse[1] == "clear\n".to_string() {
+            commands::clear_command();
+            *curent_line += 1;
+            return Ok(());
+        }
+    }
+    // println!("{:#?}", vec);
+    let text_in_line = &parse[1];
+    Err(KhaInterpreterErro::new(
+        ErroType::CommondNotFound,
+        *curent_line,
+        text_in_line.to_string(),
+    ))
 }
 
 #[cfg(test)]
