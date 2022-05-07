431b784cd10cbe1975de577d5c5f0906a3bbd33b
EmadDeve20 <emaddeve20@gmail.com>
2022-05-07 05:44:49 +0430

    update and using the new functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use kha::{get_plain_text, line_reader, ProgramFileConfig};
+use kha::{interpreter, ProgramFileConfig, new_empy_programing_config, online_interpreter};
 use std::{env, io, process};
 
 fn main() {
@@ -10,7 +10,7 @@ fn main() {
             process::exit(1);
         });
 
-        if let Err(e) = line_reader(&mut program_file) {
+        if let Err(e) = interpreter(&mut program_file) {
             eprintln!("{}", e.get_format_message());
             process::exit(1);
         }
@@ -18,12 +18,18 @@ fn main() {
 }
 
 fn run_online_interpreter() {
+    let mut code = new_empy_programing_config();
     loop {
         // TODO: is why this [In] can not print first? we must fix it
         // println!("[In]: ");
         let mut text = String::new();
         io::stdin().read_line(&mut text).expect("sonething wrong!");
-        print!("[OutPut]: ");
-        get_plain_text(text);
+        code.new_line(text);
+        if let Err(e) = online_interpreter(&mut code) {
+            eprintln!("{}", e.get_format_message());
+            continue;
+        }
+        // print!("[OutPut]: ");
+        // get_plain_text(text);
     }
 }
