8679b2175d7707f9022a9dc337a86cfb0cb4d0b4
EmadDeve20 <emaddeve20@gmail.com>
2022-05-18 17:29:45 +0430

    create the new mod for kha errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,8 @@
 use std::{env, error::Error, fs, thread, time::Duration, vec};
+use crate::kha_error::{ErroType, KhaInterpreterErro};
 
 mod commands;
-
-#[derive(Debug)]
-pub enum ErroType {
-    CommondNotFound,
-}
-#[derive(Debug)]
-pub struct KhaInterpreterErro {
-    type_of_erro: ErroType,
-    line: usize,
-    text_of_line: String,
-}
-
-impl KhaInterpreterErro {
-    pub fn new(type_of_erro: ErroType, line: usize, text_of_line: String) -> KhaInterpreterErro {
-        KhaInterpreterErro {
-            type_of_erro,
-            line,
-            text_of_line,
-        }
-    }
-
-    pub fn get_format_message(&self) -> String {
-        format!(
-            "{:?}: {} in {} line",
-            self.type_of_erro, self.text_of_line, self.line
-        )
-    }
-}
+mod kha_error;
 
 pub struct ProgramFileConfig {
     syntax: Vec<String>,
